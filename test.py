af4fb24f7b28fe82d29bfa546ee88730dcc88afb
stasbadey <stas5511665@gmail.com>
2023-10-22 22:32:10 +0300

    update model structure

--- test.py
+++ test.py
@@ -36,9 +36,10 @@ plt.show()
 data = data.map(lambda x, y: (x / 255, y))
 data.as_numpy_iterator().next()
 
-train_size = int(len(data) * .7)
-val_size = int(len(data) * .2)+1
-test_size = int(len(data) * .1)+1
+train_size = int(len(data) * 0.7)
+val_size = int(len(data) * 0.2)+1
+test_size = int(len(data) * 0.1)+1
+print(train_size, val_size, test_size)
 
 train = data.take(train_size)
 val = data.skip(train_size).take(val_size)
@@ -46,24 +47,24 @@ test = data.skip(train_size + val_size).take(test_size)
 
 model = Sequential()
 
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(256, 256, 3)))
-model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(16, (3, 3), 1,  activation='relu', input_shape=(256, 256, 3)))
+model.add(MaxPooling2D())
 
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(32, (3, 3), 1, activation='relu'))
+model.add(MaxPooling2D())
 
-model.add(Conv2D(128, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2,2)))
+model.add(Conv2D(16, (3, 3), 1, activation='relu'))
+model.add(MaxPooling2D())
 
 model.add(Flatten())
-model.add(Dense(64, activation='relu'))
+model.add(Dense(256, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
 model.compile(optimizer='adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy'])
 
 logdir = 'logs'
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
-hist = model.fit(train, epochs=20, validation_data=val, callbacks=[tensorboard_callback])
+hist = model.fit(train, epochs=10, validation_data=val, callbacks=[tensorboard_callback])
 
 fig = plt.figure()
 plt.plot(hist.history['loss'], color='teal', label='loss')
@@ -90,4 +91,4 @@ for batch in test.as_numpy_iterator():
     re.update_state(y, yhat)
     acc.update_state(y, yhat)
 
-model.save(os.path.join('models', 'lowclassifier.h5'))
+model.save(os.path.join('models', 'experimental.h5'))
